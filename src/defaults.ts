de048855fa0d16c5ca6c374f3afea482414748ab
hiroshi fukuda <dada@sygnas.jp>
2022-04-18 23:16:41 +0900

    feat: オプションのデフォルト値を外に出した

---
+++ src/defaults.ts
@@ -0,0 +1,12 @@
+const vueModalOption = {
+  closeBtnText: 'X',
+  classModal: 'c-modal',
+  classBg: 'c-modal__bg',
+  classSlide: 'c-modal__slide',
+  classContent: 'c-modal__content',
+  classClose: 'c-modal__close-btn',
+  styleBgColor: 'rgba(0, 0, 0, .7)',
+  styleZIndex: 10000,
+};
+
+export { vueModalOption };
