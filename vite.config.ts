dbb7fc4fb600d9c2cd4d441194fdc272ce056cff
hiroshi fukuda <dada@sygnas.jp>
2022-06-11 03:59:04 +0900

    fix: Vite設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 // https://zenn.dev/drop_table_user/articles/7b043bef6cec29
 // https://miyauchi.dev/ja/posts/lib-vite-tailwindcss/
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import { peerDependencies, dependencies } from './package.json'
+import { peerDependencies, dependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,17 +17,15 @@ export default defineConfig({
     },
     rollupOptions: {
       external: [...Object.keys(peerDependencies), ...Object.keys(dependencies)],
-      output: {
-        external: ['vue'],
-      },
     },
     target: 'esnext',
     sourcemap: true,
   },
   resolve: {
+    dedupe: ['vue'],
     alias: {
       '~': resolve(__dirname, 'src'),
     },
   },
   plugins: [vue()],
-})
+});
